d156fc03476f0e9acc93ec5a793d7db3f4214266
innowantai <innowantai@gmail.com>
2018-10-12 23:06:41 +0800

    ZipToAnaconda will done 2018/10/12 23:05

--- 06_ZipCompression/ZipCompression/Program.cs
+++ 06_ZipCompression/ZipCompression/Program.cs
@@ -13,22 +13,141 @@ namespace ZipCompression
         static void Main(string[] args)
         {
 
-            string start = @"C:\Users\Wantai\Desktop";
-            string Zip = @"C:\Users\Wantai\Desktop\Lib.zip";
-            using (Stream stream = File.OpenRead(Zip))
-            {
-                var reader = ReaderFactory.Open(stream);
-                while (reader.MoveToNextEntry())
-                {
-                    if (!reader.Entry.IsDirectory)
-                    {
-                        Console.WriteLine(reader.Entry.Key);
-                        reader.WriteEntryToDirectory(start, new SharpCompress.Common.ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
-                    }
-                }
-            }
+            string targetPath = "";
+            bool flag = getAnacondaPath(ref targetPath);
+
+
+            //if (flag)
+            //{
+            //    string start = targetPath ;
+            //    string Zip = @"C:\Users\Wantai\Desktop\Lib.zip";
+            //    using (Stream stream = File.OpenRead(Zip))
+            //    {
+            //        var reader = ReaderFactory.Open(stream);
+            //        while (reader.MoveToNextEntry())
+            //        {
+            //            if (!reader.Entry.IsDirectory)
+            //            {
+            //                Console.WriteLine(reader.Entry.Key);
+            //                reader.WriteEntryToDirectory(start, new SharpCompress.Common.ExtractionOptions() { ExtractFullPath = true, Overwrite = true });
+            //            }
+            //        }
+            //    }
+
+            //}
+
+
+
 
 
         }
+        
+        private static bool getAnacondaPath(ref string targetPath)
+        {
+            string[] CTARGETS = new string[] { @"C:\Program Files", @"C:\Program Files (x86)", @"C:\Users" };
+            List<string> Res = new List<string>();
+            Res.Add(@"C:\Program Files (x86)");
+            Res.Add(@"C:\Users");
+            Res.Add(@"C:\Program Files");
+            Res.Add(@"D:\");
+            Res.Add(@"E:\");
+
+            bool flag = false;
+            for (int i = 0; i < 3; i++) Res = GetPaths(Res, ref flag);
+
+            List<string> anaPath = new List<string>();
+            foreach (var ss in Res)
+            {
+                if (ss.IndexOf("Anaconda") != -1)
+                {
+                    anaPath.Add(ss);
+                }
+            }
+             
+            if (flag)
+            {
+                foreach (var ana in anaPath)
+                {
+                    if (CkeckAnacondaFolder(ana))
+                    {
+                        targetPath = ana;
+                        break;
+                    }
+                }
+            }
+            flag = targetPath == "" ? false : true;
+             
+
+            return flag;
+
+        }
+
+        private static bool CkeckAnacondaFolder(string path)
+        {
+            string[] ckeckStr = new string[] { "conda-meta", "Scripts", "Lib" };
+            List<string> check = getFolders(path, new List<string>());
+            int kk = 0;
+            foreach (string cc in ckeckStr)
+            {
+                foreach (string ck in check)
+                {
+                    if (ck.IndexOf(cc) != -1)
+                    {
+                        kk = kk + 1;
+                        break;
+                    }
+                }
+            }
+            return kk == 3 ? true : false;
+        }
+
+        private static List<string> GetPaths(List<string> Res,ref bool flag)
+        {
+            List<string> nRes = new List<string>();
+            foreach (string ss in Res)
+            {
+                if (ss.IndexOf("Anaconda") != -1) 
+                {
+                    //nRes.Clear();
+                    nRes.Add(ss);
+                    flag = true;
+                }
+                try
+                {
+                    nRes = getFolders(ss, nRes);
+                }
+                catch (System.Exception excpt)
+                {
+                }
+            }
+            return nRes;
+
+        }
+         
+
+
+        private static List<string> getFolders(string path, List<string> Res)
+        { 
+            foreach (var ss in System.IO.Directory.GetDirectories(@path))
+            {
+                if (ss.IndexOf(".") == -1)
+                {
+                    Res.Add(ss);
+                }
+
+            }
+            //showRes(Res);
+            return Res;
+
+        }
+         
+
+        private static void showRes(List<string> res)
+        {
+            foreach (var ss in res) 
+                Console.WriteLine(ss); 
+        }
+         
+
     }
 }
